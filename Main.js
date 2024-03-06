6f0f458a27cc2c9629c3b1f13febcf792367bc46
karan johal <johalkaran09@gmail.com>
2024-03-06 18:05:52 -0500

    lab 6 forms complete
    lab 6 forms done

--- Main.js
+++ Main.js
@@ -24,3 +24,18 @@ window.onclick = function(event) {
     }
 }
 */
+
+window.onload=function(){
+    var user = document.getElementById("username");
+    console.log(user.value); // the user input value.
+    console.log(user.type); // the type of the form element.
+    }
+
+function inputHandler(){
+      var myvalue = document.getElementById("myinput").value;
+      document.getElementById("emptyParagraph").innerHTML = "Your Input:" + myvalue;
+}
+
+localStorage.setItem("username", "marijn"); // store the items 
+console.log(localStorage.getItem("username"));// → marijn 
+localStorage.removeItem("username"); // remove from the localstorage
